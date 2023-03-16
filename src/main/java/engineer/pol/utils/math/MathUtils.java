f569581360bd41b63b11f84e44d564543b563d3e
Pol Vallverdu <pvallverdualcaide@gmail.com>
2023-03-16 23:30:04 +0100

    Better MathUtils

--- src/main/java/engineer/pol/utils/math/MathUtils.java
+++ src/main/java/engineer/pol/utils/math/MathUtils.java
@@ -1,37 +1,78 @@
 package engineer.pol.utils.math;
 
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
+
 public class MathUtils {
 
+    /**
+     * Linear interpolation between two values
+     *
+     * @param a Value at t = 0
+     * @param b Value at t = 1
+     * @param t Interpolation value
+     * @return Interpolated value
+     */
     public static double lerp(double a, double b, double t) {
         return a + (b - a) * t;
     }
 
-    public static double[] calculateCuttingPoint(double x1, double y1, double z1, double angle1, double x2, double y2, double z2, double angle2) {
-        double[] vector1 = new double[]{Math.cos(angle1), Math.sin(angle1), 0};
-        double[] vector2 = new double[]{Math.cos(angle2), Math.sin(angle2), 0};
+    /**
+     * Calculates where two vectors intersect in a 3D space.
+     *
+     * @param pos1 Position of the first vector
+     * @param rot1 Rotation of the first vector
+     * @param pos2 Position of the second vector
+     * @param rot2 Rotation of the second vector
+     * @return The point where the two vectors intersect
+     */
+    public static Vec3d calculateCuttingPoint(Vec3d pos1, Vec2f rot1, Vec3d pos2, Vec2f rot2) {
+        double x1 = pos1.getX();
+        double y1 = pos1.getY();
+        double z1 = pos1.getZ();
+        double pitch1 = Math.toRadians(rot1.x);
+        double yaw1 = Math.toRadians(rot1.y);
 
-        double[] crossProduct = new double[]{
-                vector1[1] * vector2[2] - vector1[2] * vector2[1],
-                vector1[2] * vector2[0] - vector1[0] * vector2[2],
-                vector1[0] * vector2[1] - vector1[1] * vector2[0]
-        };
+        double x2 = pos2.getX();
+        double y2 = pos2.getY();
+        double z2 = pos2.getZ();
+        double pitch2 = Math.toRadians(rot2.x);
+        double yaw2 = Math.toRadians(rot2.y);
 
-        double dotProduct1 = x1 * crossProduct[0] + y1 * crossProduct[1] + z1 * crossProduct[2];
-        double dotProduct2 = x2 * crossProduct[0] + y2 * crossProduct[1] + z2 * crossProduct[2];
-        double scalar1 = dotProduct1 / (Math.pow(vector1[0], 2) + Math.pow(vector1[1], 2) + Math.pow(vector1[2], 2));
-        double scalar2 = dotProduct2 / (Math.pow(vector2[0], 2) + Math.pow(vector2[1], 2) + Math.pow(vector2[2], 2));
+        double a1 = Math.sin(pitch1);
+        double b1 = Math.cos(pitch1) * Math.sin(yaw1);
+        double c1 = Math.cos(pitch1) * Math.cos(yaw1);
 
-        return new double[]{
-                x1 + scalar1 * vector1[0],
-                y1 + scalar1 * vector1[1],
-                z1 + scalar1 * vector1[2]
-        };
+        double a2 = Math.sin(pitch2);
+        double b2 = Math.cos(pitch2) * Math.sin(yaw2);
+        double c2 = Math.cos(pitch2) * Math.cos(yaw2);
+
+        double d = (a1 * b2) - (a2 * b1);
+        double e = (a1 * c2) - (a2 * c1);
+        double f = (b1 * c2) - (b2 * c1);
+
+        double g = Math.sqrt((d * d) + (e * e) + (f * f));
+
+        double x = ((y1 * f) - (z1 * e) - (y2 * f) + (z2 * e)) / g + x1;
+        double y = ((z1 * d) - (x1 * f) - (z2 * d) + (x2 * f)) / g + y1;
+        double z = ((x1 * e) - (y1 * d) - (x2 * e) + (y2 * d)) / g + z1;
+
+        return new Vec3d(x, y, z);
     }
 
-    public static double[] slerp(double x1, double y1, double z1, double x2, double y2, double z2, double cx, double cy, double cz, double t) {
+    /**
+     * Linear spherical interpolation between two points in a 3D space, and a center point.
+     *
+     * @param pos1 Position when t = 0
+     * @param pos2 Position when t = 1
+     * @param center Center point
+     * @param t Interpolation value
+     * @return Interpolated position
+     */
+    public static Vec3d slerp(/*double x1, double y1, double z1*/Vec3d pos1, /*double x2, double y2, double z2*/Vec3d pos2, /*double cx, double cy, double cz*/Vec3d center, double t) {
         // Calculate the vectors from the center point to each endpoint
-        double[] v1 = new double[] { x1 - cx, y1 - cy, z1 - cz };
-        double[] v2 = new double[] { x2 - cx, y2 - cy, z2 - cz };
+        double[] v1 = new double[] { pos1.getX() - center.getX(), pos1.getY() - center.getY(), pos1.getZ() - center.getZ() };
+        double[] v2 = new double[] { pos2.getX() - center.getX(), pos2.getY() - center.getY(), pos2.getZ() - center.getZ() };
 
         // Normalize the vectors
         double v1Len = Math.sqrt(v1[0] * v1[0] + v1[1] * v1[1] + v1[2] * v1[2]);
@@ -57,15 +98,13 @@ public class MathUtils {
         double weight2 = Math.sin(t * angle) / sinAngle;
 
         // Calculate the slerped point
-        double[] slerp = new double[] {
-                weight1 * x1 + weight2 * x2,
-                weight1 * y1 + weight2 * y2,
-                weight1 * z1 + weight2 * z2
-        };
+        Vec3d slerp = new Vec3d(
+                weight1 * v1[0] + weight2 * v2[0],
+                weight1 * v1[1] + weight2 * v2[1],
+                weight1 * v1[2] + weight2 * v2[2]
+        );
 
         return slerp;
     }
-
-
 
 }
