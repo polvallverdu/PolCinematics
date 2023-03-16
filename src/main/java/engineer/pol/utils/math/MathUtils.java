74fb3a31c2c2a6b69133e0b604b36b3899b88248
Pol Vallverdu <pvallverdualcaide@gmail.com>
2023-03-16 23:25:34 +0100

    Useless function

--- src/main/java/engineer/pol/utils/math/MathUtils.java
+++ src/main/java/engineer/pol/utils/math/MathUtils.java
@@ -1,13 +1,6 @@
 package engineer.pol.utils.math;
 
-import java.awt.*;
-import java.awt.geom.Point2D;
-
 public class MathUtils {
-
-    public static Point2D lerp(Point2D a, Point2D b, double t) {
-        return new Point2D.Double(a.getX() + (b.getX() - a.getX()) * t, a.getY() + (b.getY() - a.getY()) * t);
-    }
 
     public static double lerp(double a, double b, double t) {
         return a + (b - a) * t;
