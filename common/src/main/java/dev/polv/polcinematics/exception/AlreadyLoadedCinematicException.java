b951e60916721bf99c21836cf9e0d2f5e0fa9be4
polvallverdu <pvallverdualcaide@gmail.com>
2023-04-30 23:26:44 +0200

    Created AlreadyLoadedCinematicException

---
+++ common/src/main/java/dev/polv/polcinematics/exception/AlreadyLoadedCinematicException.java
@@ -0,0 +1,7 @@
+package dev.polv.polcinematics.exception;
+
+public class AlreadyLoadedCinematicException extends RuntimeException {
+    public AlreadyLoadedCinematicException(String message) {
+        super(message);
+    }
+}
