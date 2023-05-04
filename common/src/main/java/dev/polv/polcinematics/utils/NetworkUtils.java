c503da37a7fc07fc2e3b1a9a59ec38a8c14ca011
polvallverdu <pvallverdualcaide@gmail.com>
2023-05-04 20:18:34 +0200

    Fixed netty issue RefCef: 0

--- common/src/main/java/dev/polv/polcinematics/utils/NetworkUtils.java
+++ common/src/main/java/dev/polv/polcinematics/utils/NetworkUtils.java
@@ -5,7 +5,7 @@ import net.minecraft.network.PacketByteBuf;
 
 public class NetworkUtils {
 
-    public final static PacketByteBuf EMPTY_BUFFER = new PacketByteBuf(Unpooled.buffer());
+    public final static PacketByteBuf EMPTY_BUFFER = new PacketByteBuf(Unpooled.EMPTY_BUFFER);
 
     public static PacketByteBuf createBuffer() {
         return new PacketByteBuf(Unpooled.buffer());
