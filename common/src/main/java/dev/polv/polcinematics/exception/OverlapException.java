977d54bafe2f18650a67f4d385f32e46457a7c81
polvallverdu <pvallverdualcaide@gmail.com>
2023-05-04 20:00:53 +0200

    added " "

--- common/src/main/java/dev/polv/polcinematics/exception/OverlapException.java
+++ common/src/main/java/dev/polv/polcinematics/exception/OverlapException.java
@@ -5,7 +5,7 @@ import dev.polv.polcinematics.cinematic.compositions.core.Timeline;
 public class OverlapException extends RuntimeException {
 
     public OverlapException(Timeline.WrappedComposition wc, Timeline.WrappedComposition wcOverlapping) {
-        super("Composition " + wc.getComposition().getName() + " (" + wc.getUUID() + ") overlaps with " + wcOverlapping.getComposition().getName() + "(" + wcOverlapping.getUUID() + ")");
+        super("Composition " + wc.getComposition().getName() + " (" + wc.getUUID() + ") overlaps with " + wcOverlapping.getComposition().getName() + " (" + wcOverlapping.getUUID() + ")");
     }
 
 }
